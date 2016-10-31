bca6c11f282483ac55543520d1757422e78be040
Benoit Quette <benoit.quette@gmail.com>
2016-10-31 22:46:45 +0100

    added icon and changed the theme

--- app/styles.js
+++ app/styles.js
@@ -1,9 +1,18 @@
-import {deepPurple500, deepOrange500} from "material-ui/styles/colors";
+import {
+  indigo500,
+  indigo700,
+  indigo100,
+  lime500,
+  grey900
+} from "material-ui/styles/colors";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 
 export const theme = getMuiTheme({
   palette: {
-    primary1Color: deepPurple500,
-    accent1Color: deepOrange500
+    primary1Color: indigo500,
+    primary2Color: indigo700,
+    primary3Color: indigo100,
+    accent1Color: lime500,
+    textColor: grey900
   }
 });
