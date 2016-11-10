08ecf96c2d0fb298ff6f9887dd952d99a1fe5f1b
Benoit Quette <benoit.quette@gmail.com>
2016-11-10 15:10:52 +0100

    added backgroundcolor

--- app/components/Canvas.jsx
+++ app/components/Canvas.jsx
@@ -2,10 +2,13 @@ import React, {Component} from "react";
 import Title from "@components/Title";
 import Text from "@components/Text";
 import withWidth, {SMALL} from 'material-ui/utils/withWidth';
+import muiThemeable from 'material-ui/styles/muiThemeable';
 
 @withWidth()
+@muiThemeable()
 export default class Canvas extends Component {
   static propTypes = {
+    muiTheme: React.PropTypes.object.isRequired,
     drawerOpen: React.PropTypes.bool.isRequired,
     title: React.PropTypes.string,
     text: React.PropTypes.string,
@@ -18,6 +21,8 @@ export default class Canvas extends Component {
     if (this.props.drawerOpen && this.props.width !== SMALL) {
       paddingDrawer = styles.drawerOpened;
     }
+    let contentStyle = styles.content;
+    contentStyle.backgroundColor = this.props.muiTheme.palette.primary3Color;
     return (
       <div>
         <div style={paddingDrawer}>
@@ -27,7 +32,7 @@ export default class Canvas extends Component {
           {this.props.text !== undefined && (
             <Text text={this.props.text}/>
           )}
-          <div style={styles.content}>
+          <div style={contentStyle}>
             {this.props.children}
           </div>
         </div>
