824ec9341aed53ee222ac387a93d48c1297d3c11
Benoit Quette <benoit.quette@gmail.com>
2016-12-20 17:48:12 +0100

    changed ordering of list of audits to DESC

--- app/modules/log/Log.jsx
+++ app/modules/log/Log.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import Canvas from "@components/Canvas";
 import {connect} from "react-redux";
-import LogToolbar from './LogToolbar';
 import _ from 'lodash';
 import moment from 'moment';
 import {fetchAuditsList} from './actions';
@@ -40,42 +39,42 @@ export default class Log extends Component {
 
   render() {
     // first, we group by date because we want to show one row per date
-    const resultsByDate = _.groupBy(this.props.results, result => {
-      return moment(result.timestamp).format(constants.shortDateFormat);
-    });
-    const items = _.map(resultsByDate, results => {
-      // if we have more than one audit on one date, we take the last one
-      const date = _.max(results, result => {
-        return moment(result.timestamp).valueOf();
-      }).timestamp;
-      const shortDate = moment(date).format(constants.shortDateFormat);
-      const longDate = moment(date).format(constants.longDateFormat);
-      // we take the list of categories for each results....
-      const categoriesList = _.map(results, result => {
-        return result.category;
-      });
-      // and we deduplicate it. That will allow us to keep track of a date
-      // when multiple audits were done and flag it to the user
-      const categories = _.uniq(categoriesList);
-      // final date: the number of URLs crawled
-      const urlCount = Object.keys(_.groupBy(results, result => {
-        return result.url;
-      })).length;
-      return (
-        <LogListItem
-          key={shortDate}
-          shortDate={shortDate}
-          longDate={longDate}
-          urlCount={urlCount}
-          multipleAuditsWarning={categories.length !== categoriesList.length}
-          onClick={this.onClick.bind(this, shortDate)}
-          categories={categories}
-        />
-      );
-    });
-    const toolbar = (
-      <LogToolbar/>
-    );
+    const items = _.chain(this.props.results)
+      .groupBy(result => {
+        return moment(result.timestamp).format(constants.shortDateFormat);
+      })
+      .map(results => {
+        // if we have more than one audit on one date, we take the last one
+        const date = _.max(results, result => {
+          return moment(result.timestamp).valueOf();
+        }).timestamp;
+        const shortDate = moment(date).format(constants.shortDateFormat);
+        const longDate = moment(date).format(constants.longDateFormat);
+        // we take the list of categories for each results....
+        const categoriesList = _.map(results, result => {
+          return result.category;
+        });
+        // and we deduplicate it. That will allow us to keep track of a date
+        // when multiple audits were done and flag it to the user
+        const categories = _.uniq(categoriesList);
+        // final date: the number of URLs crawled
+        const urlCount = Object.keys(_.groupBy(results, result => {
+          return result.url;
+        })).length;
+        return (
+          <LogListItem
+            key={shortDate}
+            shortDate={shortDate}
+            longDate={longDate}
+            urlCount={urlCount}
+            multipleAuditsWarning={categories.length !== categoriesList.length}
+            onClick={this.onClick.bind(this, shortDate)}
+            categories={categories}
+          />
+        );
+      })
+      .reverse()
+      .value();
     return (
       <Canvas toolbar={toolbar} drawerOpen={this.props.drawerOpen}>
         <LogList onClick={this.onCellClick.bind(this)}>
