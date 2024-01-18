7530abc952a59c4115262bd7e6d464da8017b0f4
Benoit Quette <benoit.quette@gmail.com>
2024-01-18 16:12:48 +0100

    cleanup nav

--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -3,14 +3,13 @@ import pages from './pages';
 import projects from './projects';
 import dashboard from './dashboard';
 import utilities from './utilities';
-import support from './support';
 import settings from './settings';
 
 // ==============================|| MENU ITEMS ||============================== //
 
 const menuItems = (allProjects) => {
   return {
-    items: [dashboard, projects(allProjects), settings, pages, utilities, support]
+    items: [dashboard, projects(allProjects), settings, pages, utilities]
   };
 };
 
