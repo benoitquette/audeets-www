1ff85edd952be0c86c9149828a550ac82f466563
Benoit Quette <benoit.quette@gmail.com>
2024-02-05 23:17:09 +0100

    added the limit of 5 projects

--- src/pages/settings-projects/index.jsx
+++ src/pages/settings-projects/index.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
 // material-ui
-import { Button, Grid, Typography, Link } from '@mui/material';
+import { Button, Grid, Typography, Link, Tooltip } from '@mui/material';
 
 // project import
 import ProjectsTable from './ProjectsTable';
@@ -10,11 +10,15 @@ import MainCard from '~/components/MainCard';
 // types
 import { useGetProjectsQuery } from '~/store/reducers/projectsApi';
 
+// assets
+import { WarningOutlined } from '@ant-design/icons';
+
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
 const SettingsProject = () => {
   const { data: projects } = useGetProjectsQuery();
   const navigate = useNavigate();
+  const limitReached = projects.length >= 5;
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
       <Grid item xs={12}>
@@ -23,7 +27,19 @@ const SettingsProject = () => {
             <Typography variant="h5">Projects</Typography>
           </Grid>
           <Grid item sx={{ mr: 0.5 }}>
-            <Button component={Link} size="small" variant="contained" onClick={() => navigate('/settings/projects/create')}>
+            {limitReached && (
+              <Tooltip title="Max number of 5 projects reached">
+                <WarningOutlined style={{ color: 'red' }} />
+              </Tooltip>
+            )}
+            <Button
+              sx={{ ml: 1 }}
+              disabled={limitReached}
+              component={Link}
+              size="small"
+              variant="contained"
+              onClick={() => navigate('/settings/projects/create')}
+            >
               Create project
             </Button>
           </Grid>
