8eabd059d026dbdee6a30c4bd5f61f9f0d1fc704
Benoit Quette <benoit.quette@gmail.com>
2024-03-09 19:29:52 +0100

    forma

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,7 @@
-// third-party
 import { configureStore } from '@reduxjs/toolkit';
-
-// project import
 import menu from './reducers/menu';
 import { projectsApi } from './reducers/projects-api';
 import user from './reducers/user';
-
-// ==============================|| REDUX TOOLKIT - MAIN STORE ||============================== //
 
 const store = configureStore({
   reducer: {
