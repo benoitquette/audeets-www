b42617416aa7e7f415a62e752bc4e80798430937
Benoit Quette <benoit.quette@gmail.com>
2024-03-15 12:25:36 +0100

    try and fix the issue with cookies

--- src/components/Protected/Protected.jsx
+++ src/components/Protected/Protected.jsx
@@ -10,18 +10,17 @@ const Protected = ({ children }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  const { user, status } = useSelector((state) => state.user);
-  const sessionCookie = Cookies.get(sessionCookieName);
 
+  const { user, status } = useSelector((state) => state.user);
   if (status === 'failed') Cookies.remove(sessionCookieName);
 
   useEffect(() => {
-    if (!sessionCookie) {
+    if (!Cookies.get(sessionCookieName)) {
       navigate(`/login?returnTo=${location.pathname}`);
     } else if (!user && ['succeeded', 'idle'].includes(status)) {
       dispatch(fetchUser());
     }
-  }, [dispatch, location.pathname, navigate, sessionCookie, status, user]);
+  }, [dispatch, location.pathname, navigate, status, user]);
 
   if (user) return children;
 };
