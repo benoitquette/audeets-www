13f5f6fdecd2316cb4f0beaa8fd7991bd5d66717
Benoit Quette <benoit.quette@gmail.com>
2024-03-17 09:54:15 +0100

    added globals to window

--- vitest-setup.js
+++ vitest-setup.js
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom/vitest';
 import { beforeAll } from 'vitest';
 
 beforeAll(() => {
-  globalThis.apiProjectsUrl = '';
-  globalThis.apiUsersUrl = '';
-  globalThis.apiReportsUrl = '';
+  window.apiProjectsUrl = '';
+  window.apiUsersUrl = '';
+  window.apiReportsUrl = '';
 });
