764977a0b3e1cc41ed1ef1113185b206fe7252c4
Benoit Quette <benoit.quette@gmail.com>
2024-03-17 11:59:13 +0100

    debug

--- vitest-setup.js
+++ vitest-setup.js
@@ -1,12 +1,11 @@
 import '@testing-library/jest-dom/vitest';
 import { beforeAll, vi } from 'vitest';
-import config from '~/config';
 
 beforeAll(() => {
   window.apiProjectsUrl = 'fake';
   globalThis.apiProjectsUrl = 'fake';
   vi.stubGlobal('apiProjectsUrl', 'fake');
   global.apiProjectsUrl = 'fake';
-  console.log('apiProjectsUrl: ' + apiProjectsUrl);
-  console.log('urlApiProjects: ' + config.urlApiProjects);
+  //   console.log('apiProjectsUrl: ' + apiProjectsUrl);
+  //   console.log('urlApiProjects: ' + config.urlApiProjects);
 });
