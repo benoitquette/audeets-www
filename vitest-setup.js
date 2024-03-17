d1e741435c017c54c86fa1cd108a40c4fe1c96a1
Benoit Quette <benoit.quette@gmail.com>
2024-03-17 09:50:26 +0100

    added global config vars to all tests

--- vitest-setup.js
+++ vitest-setup.js
@@ -2,4 +2,7 @@ import '@testing-library/jest-dom/vitest';
 import { beforeAll } from 'vitest';
 
 beforeAll(() => {
+  globalThis.apiProjectsUrl = '';
+  globalThis.apiUsersUrl = '';
+  globalThis.apiReportsUrl = '';
 });
